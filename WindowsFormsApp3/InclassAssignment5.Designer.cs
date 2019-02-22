e5a3736d6248eb1f410ca5d431b67b11423f0582
Ivan Rivas <IRivas1@my.gcu.edu>
2019-02-22 15:36:59 -0700

    Update

--- WindowsFormsApp3/InclassAssignment5.Designer.cs
+++ WindowsFormsApp3/InclassAssignment5.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btn1_Calculate = new System.Windows.Forms.Button();
             this.label1_Output = new System.Windows.Forms.Label();
+            this.label1_answer = new System.Windows.Forms.Label();
+            this.label1_userNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1_NumofTerms
@@ -40,25 +42,26 @@
             this.label1_NumofTerms.AutoSize = true;
             this.label1_NumofTerms.Location = new System.Drawing.Point(48, 33);
             this.label1_NumofTerms.Name = "label1_NumofTerms";
-            this.label1_NumofTerms.Size = new System.Drawing.Size(104, 17);
+            this.label1_NumofTerms.Size = new System.Drawing.Size(113, 17);
             this.label1_NumofTerms.TabIndex = 0;
-            this.label1_NumofTerms.Text = "End # of terms:";
+            this.label1_NumofTerms.Text = "Enter # of terms:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 33);
+            this.textBox1.Location = new System.Drawing.Point(167, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
             // 
             // btn1_Calculate
             // 
-            this.btn1_Calculate.Location = new System.Drawing.Point(67, 110);
+            this.btn1_Calculate.Location = new System.Drawing.Point(51, 112);
             this.btn1_Calculate.Name = "btn1_Calculate";
-            this.btn1_Calculate.Size = new System.Drawing.Size(85, 29);
+            this.btn1_Calculate.Size = new System.Drawing.Size(93, 39);
             this.btn1_Calculate.TabIndex = 2;
             this.btn1_Calculate.Text = "Calculate";
             this.btn1_Calculate.UseVisualStyleBackColor = true;
+            this.btn1_Calculate.Click += new System.EventHandler(this.btn1_Calculate_Click);
             // 
             // label1_Output
             // 
@@ -69,11 +72,29 @@
             this.label1_Output.TabIndex = 3;
             this.label1_Output.Text = "Approximate value of PI after";
             // 
+            // label1_answer
+            // 
+            this.label1_answer.AutoSize = true;
+            this.label1_answer.Location = new System.Drawing.Point(48, 231);
+            this.label1_answer.Name = "label1_answer";
+            this.label1_answer.Size = new System.Drawing.Size(0, 17);
+            this.label1_answer.TabIndex = 5;
+            // 
+            // label1_userNum
+            // 
+            this.label1_userNum.AutoSize = true;
+            this.label1_userNum.Location = new System.Drawing.Point(239, 200);
+            this.label1_userNum.Name = "label1_userNum";
+            this.label1_userNum.Size = new System.Drawing.Size(0, 17);
+            this.label1_userNum.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(899, 527);
+            this.Controls.Add(this.label1_answer);
+            this.Controls.Add(this.label1_userNum);
             this.Controls.Add(this.label1_Output);
             this.Controls.Add(this.btn1_Calculate);
             this.Controls.Add(this.textBox1);
@@ -92,6 +113,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btn1_Calculate;
         private System.Windows.Forms.Label label1_Output;
+        private System.Windows.Forms.Label label1_answer;
+        private System.Windows.Forms.Label label1_userNum;
     }
 }
 
---
+++ WindowsFormsApp3/InclassAssignment5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Calculate_Click(object sender, EventArgs e)
+        {
+            double num = 4.0, denom = 1.0, sum = 0.0;
+            string userNum = textBox1.Text;
+            int userNum1 = int.Parse(userNum);
+            label1_userNum.Text ="" + userNum.ToString() + "  term(s).";
+
+            for (int i = 1; i <= userNum1; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += num / denom;
+                }
+
+                else
+                {
+                    sum -= num / denom;
+                }
+
+                denom += 2.0;
+            }
+
+            label1_answer.Text = " =    " + sum.ToString();
+
+
+        }
+    }
+}
